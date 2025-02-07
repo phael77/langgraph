0732d3b4c5052315cb87abaa3d47630687dd3174
phael77 <rapha555lima@gmail.com>
2025-02-06 23:28:56 -0300

    criação da logica de um sistema multi agente

--- multi_agent.py
+++ multi_agent.py
@@ -1,5 +1,15 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from typing import Annotated
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+from langchain_experimental.utilities import PythonREPL
+
+tavily_tool = TavilySearchResults(max_results=5)
+
+repl = PythonREPL()
 
 load_dotenv()
 
@@ -10,3 +20,19 @@ try:
         print("API key is loaded sucessfully")
 except Exception as e:
     print(f"Erro {e}")
+
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."]):
+    """Use this to execute python code and do math. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    result_str = f"Successfully executed:\n\`\`\`python\n{code}\n\`\`\`\nStdout: {result}"
+    return result_str
+
+
+
+
