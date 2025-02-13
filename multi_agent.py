d3fac2f3e80514ac74e55a873913cc49896829e5
phael77 <rapha555lima@gmail.com>
2025-02-13 15:54:46 -0300

    langgraph test criado

--- multi_agent.py
+++ multi_agent.py
@@ -1,11 +1,18 @@
 import os
 
 from dotenv import load_dotenv
-from typing import Annotated
+from typing import Annotated, Literal
+from typing_extensions import TypedDict
 
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.tools import tool
 from langchain_experimental.utilities import PythonREPL
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+from langgraph.graph import MessagesState, END, StateGraph, START
+from langgraph.types import Command
+from langgraph.prebuilt import create_react_agent
 
 tavily_tool = TavilySearchResults(max_results=5)
 
@@ -15,11 +22,19 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.1,
+    max_tokens=None,
+    timeout=None,
+    max_retries=5
+)
+'''
 try:
     if api_key is not None:
         print("API key is loaded sucessfully")
 except Exception as e:
-    print(f"Erro {e}")
+    print(f"Erro {e}")'''
 
 @tool
 def python_repl_tool(
@@ -33,6 +48,76 @@ def python_repl_tool(
     result_str = f"Successfully executed:\n\`\`\`python\n{code}\n\`\`\`\nStdout: {result}"
     return result_str
 
+members = ["researcher", "coder"]
+
+options = members + ["FINISH"]
+
+system_prompt = (
+    "You are a supervisor tasked with managing a conversation between the"
+    f" following workers: {members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH."
+)
+
+class Router(TypedDict):
+
+    next: Literal[*options]
 
 
+class State(MessagesState):
+    
+    next: str
 
+def supervisor_node(state: State) -> Command[Literal[*members, "__end__"]]:
+    
+    messages = [
+        {"role": "system", "content": system_prompt},
+    ] + state["messages"]
+
+    response = llm.with_structured_output(Router).invoke(messages)
+    goto = response["next"]
+    if goto == "FINISH":
+        goto = END
+
+    return Command(goto=goto, update={"next": goto})
+
+research_agent = create_react_agent(llm, tools=[tavily_tool], prompt="You are a research. DO NOT do any math.")
+
+def research_node(state: State) -> Command[Literal["supervisor"]]:
+    result = research_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="researcher")
+            ]
+        },
+        goto="supervisor"
+    )
+
+code_agent = create_react_agent(llm, tools=[python_repl_tool])
+
+
+def code_node(state: State) -> Command[Literal["supervisor"]]:
+    result = code_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="coder")
+            ]
+        },
+        goto="supervisor",
+    )
+
+builder = StateGraph(State)
+builder.add_edge(START, "supervisor")
+builder.add_node("supervisor", supervisor_node)
+builder.add_node("researcher", research_node)
+builder.add_node("coder", code_node)
+graph = builder.compile()
+
+for s in graph.stream(
+    {"messages": [("user", "What's the square root of 42?")]}, subgraphs=True
+):
+    print(s)
+    print("----")
